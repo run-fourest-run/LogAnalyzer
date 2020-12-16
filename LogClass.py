8f8e1ea2d3e6f471b7ab96989f8f9eee675b5d1d
afournier <alexander.fournier@getmanta.com>
2020-12-15 18:23:24 -0700

    cleanup

--- LogClass.py
+++ LogClass.py
@@ -2,48 +2,8 @@ import os
 class Log:
     __slots__ = ('logname','phase','input_script')
 
-    def __init__(self, logname, phase, mantascript, messages):
-        self.__logname = logname
-        '''
-                self.phase = phase,
-        self.mantascript = mantascript
-        if messages is None:
-            self.messages = []
-        else:
-            self.messages = list(messages)
+    def __init__(self, license_expiration):
 
-        
-        
-        '''
-
-
-
-    @property
-    def logname(self):
-        return self.__logname
-
-
-    def __iter__(self):
-        return ((a,b,c,d,e,f) for a,b,c,d,e,f in (self.logname,self.messages,self.errors,self.warnings,self.phase,self.input_script))
-
-    def get_phase(self):
-        pass
-
-    def get_technology(self):
-        pass
-
-
-    def get_message(self,message):
-        pass
-
-    def get_warnings(self,warning):
-        pass
-
-    def get_errors(self,error):
-        pass
-
-    def get_execution_time(self):
-        pass
 
 
 
@@ -52,6 +12,8 @@ class Log:
         if dir_path is None:
             dir_path = os.path.dirname(os.path.realpath(__file__) + '\\' + filename)
         with open(dir_path) as f:
+            lines = f.readlines()
+
 
 
 
