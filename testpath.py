504597ee0ffdca13ef7a22ff6b31a4ab65e62f12
afournier <alexander.fournier@getmanta.com>
2020-12-15 18:37:03 -0700

    license_key_search function works from a high level. Next thing I need to do is make a generator function similar to this: https://stackoverflow.com/questions/9868653/find-first-sequence-item-that-matches-a-criterion

--- testpath.py
+++ testpath.py
@@ -1,3 +1,19 @@
 import os
-
+import re
 print(os.path.dirname(os.path.realpath(__file__)))
+
+f = r'C:\Users\afournier\PycharmProjects\LogAnalyzer\Logs\test.txt'
+pattern = re.compile('version')
+
+
+def license_key_search(pattern,filename):
+    license_key_matches = []
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            match = re.search(pattern,line)
+            if match:
+                license_key_matches.append(line)
+    return license_key_matches
+
+
