27a78b3a3b011a63f8ea5dde14bee9f48424375e
afournier <alexander.fournier@getmanta.com>
2020-12-22 14:38:28 -0700

    Chunking with functional programming works I think

--- utils/Splitter.py
+++ utils/Splitter.py
@@ -3,11 +3,9 @@ from itertools import chain,groupby
 
 
 
-
-delimiter = '---------------------------------------------------------------------'
-pattern = r'^Manta\sFlow\s(\w*\s\w*\s\w*)\sScenario\s\(version\s[0-9]\.[0-9][0-9]\.[0-9]\)'
-regex_pattern = re.compile(pattern)
 input_file = r'C:\Users\afournier\Desktop\testcopy3.log'
+script_name_regex_pattern = r'^Manta\sFlow\s(\w*\s\w*\s\w*)\sScenario\s\(version\s[0-9]\.[0-9][0-9]\.[0-9]\)'
+regex_pattern = re.compile(script_name_regex_pattern)
 key_function = lambda line: re.search(regex_pattern,line)
 
 
